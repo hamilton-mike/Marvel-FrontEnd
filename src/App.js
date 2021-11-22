b6143795ea440fb30ea2b4213f15502a0d7cff3f
Michael Hamilton <hamiltonisaiah9119@gmail.com>
2021-11-22 16:51:50 -0700

    get request

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import './App.css'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [hero, setHero] = useState([]);
+
+  const getMarvelCharacter = async () => {
+    try {
+      const getRequest = await fetch('http://localhost:9000/comic');
+      console.log(getRequest);
+      const json = await getRequest.json();
+      console.log(json);
+      setHero(json)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getMarvelCharacter()
+  }, [])
+
   return (
-    <div className="App">MERN APP</div>
+    <div className="App">
+      <h1>MERN APP</h1>
+    </div>
   );
 }
 
