755bede7d73a74b93af15350a48e359666af3b32
Michael Hamilton <hmisaiah30@gmail.com>
2022-02-10 00:28:54 -0700

    settimeout

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { HeroDiv, MarvelIntro, LinkWrapper } from './HeroStyles'
 import { Button } from '../HomeHero/HomeHeroStyle'
 import { useNavigate } from 'react-router-dom'
 
 const Hero = ({ video }) => {
+    const [visible, setVisible] = useState(true)
     const navigate = useNavigate();
 
     const home = () => {
         navigate('/home')
     }
+
+    setTimeout(() => {
+        setVisible(false)
+    }, 4000)
+
 
     return (
         <HeroDiv>
@@ -16,7 +22,7 @@ const Hero = ({ video }) => {
                 <source src={video} type="video/mp4" />
             </MarvelIntro>
             <LinkWrapper>
-                <Button onClick={home}>Home</Button>
+                {visible ? null : <Button onClick={home}>Home</Button>}
             </LinkWrapper>
         </HeroDiv>
     )
